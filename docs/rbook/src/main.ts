a418b13d81633596e3b81b0de559effab8fd847a
Sam Ban <sam@gpmd.co.uk>
2019-09-22 16:18:55 +0100

    remove wrong post

--- docs/rbook/src/main.ts
+++
@@ -1,7 +0,0 @@
-//main entry point
-import {bootstrap} from 'angular2/platform/browser';
-import {HTTP_PROVIDERS} from 'angular2/http';
-import {App} from './app';
-
-bootstrap(App, [HTTP_PROVIDERS])
-    .catch(err => console.error(err));
